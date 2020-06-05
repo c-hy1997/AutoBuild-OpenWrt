a1fdf19f9cd516087323d9e7aee8b7a14eda44d2
Michael Chan <57629941+c-hy1997@users.noreply.github.com>
2020-06-05 14:58:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
 
 #Add Hello World
-cd package
-cd lean
-git clone https://github.com/jerrykuku/lua-maxminddb.git 
-git clone https://github.com/jerrykuku/luci-app-vssr.git 
+#cd package
+#cd lean
+#git clone https://github.com/jerrykuku/lua-maxminddb.git 
+#git clone https://github.com/jerrykuku/luci-app-vssr.git 
